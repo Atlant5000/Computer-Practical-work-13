50b0f1276461156bc613248861eef0ff65119d8b
Maksim Mihailovskii <maximmmd13@gmail.com>
2024-12-10 09:03:33 +0200

    Add files via upload

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    /// <summary>
+    /// По номеру месяца вывести его название.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Value();
+            Console.ReadKey();
+        }
+        public static void Value()
+        {
+            int n = Input("Введите число от 1 до 12 ,month=");
+            Console.WriteLine(GetName(n));
+        }
+        public static int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+        public static string GetName(int month)
+        {
+            switch (month)
+            {
+                case 1: return "Январь";
+                case 2: return "Февраль";
+                case 3: return "Март";
+                case 4: return "Апрель";
+                case 5: return "Май";
+                case 6: return "Июнь";
+                case 7: return "Июль";
+                case 8: return "Август";
+                case 9: return "Сентябрь";
+                case 10: return "Октябрь";
+                case 11: return "Ноябрь";
+                case 12: return "Декабрь";
+                default: return "Введено неправильное число";
+            }
+        }
+    }
+}
